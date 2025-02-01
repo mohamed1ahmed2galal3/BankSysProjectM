484f8c768a0e8e1edb8ef6f2f623be6ec8c12a7f
Mohamed Ahmed Galal <mohamedahmedgalal246@gmail.com>
2025-02-01 19:21:15 +0200

    Add Validator Class

--- main.cpp
+++ main.cpp
@@ -1,15 +1,44 @@
-#include <iostream>
-using namespace std;
+#include<iostream>
+#include <cctype>
 #include<string>
 
-void printMohamed()
-{
-    cout<<"Mohamed\n";
-}
+using namespace std ;
 
-int main()
+class Validator
 {
-    string s ="Ahmed";
-    printMohamed();
-    cout<<s<<endl;
-}
+public :
+    static bool isAlphaString(string & str)
+    {
+        for(auto c : str)
+        {
+            if(!isalpha(static_cast<unsigned char>(c)))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+   static bool isValidLength(string& str) {
+    return str.length() >= 5 && str.length() <= 20;
+   }
+   static bool isValidbalance(double& balance)
+   {
+       if(balance >=1500)
+        {
+            return true ;
+        }
+        else{
+            return false;
+        }
+   }
+   static bool isValidSalay(double& salary)
+   {
+       if(salary >= 5000)
+       {
+           return true;
+       }
+       else{
+           return false;
+       }
+   }
+};
