52d29afe133935c0acfb59af5e91be54ef2ebe0b
Mohamed Ahmed Galal <mohamedahmedgalal246@gmail.com>
2025-02-14 16:47:08 +0200

    Replace DataSourceInterface by A new Class

--- DataSourceInterface .h
+++ DataSourceInterface .h
@@ -1,19 +1,1 @@
-#ifndef DATASOURCEINTERFACE_H
-#define DATASOURCEINTERFACE_H
-#include "Client.h"
-#include "Employee.h"
-#include "Admin.h"
 
-class DataSourceInterface {
-public:
-    virtual void addClient(Client) = 0;
-    virtual void addEmployee(Employee) = 0;
-    virtual void addAdmin(Admin) = 0;
-    virtual void getAllClients() = 0;
-    virtual void getAllEmployees() = 0;
-    virtual void getAllAdmins() = 0;
-    virtual void removeAllClients() = 0;
-    virtual void removeAllEmployees() = 0;
-    virtual void removeAllAdmins() = 0;
-};
-#endif // DATASOURCEINTERFACE_H
